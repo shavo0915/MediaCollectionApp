6db32321f60c69c88f5b5c6071a1804c57e7f99d
Samuel Bravo <sbravo09@gmail.com>
2013-02-14 11:24:29 -0700

    Added new js file  mediaQuery which will be used to query based on user input their respective media databases. Added initial method that will do the calls based on user input, and 2 other methods to view what media type the user selected and what they are looking for. this basic functionality was added to the Add Media page.

---
+++ MediaCollectionApp/assets/www/js/mediaQuery.js
@@ -0,0 +1,33 @@
+//Function used to call queries depending on what media the user selected.
+function queryMedia(){
+	var chosenMedia = getSelectedMediaValue();
+	
+	if(chosenMedia == "games"){
+		var queryString = getQueryString();
+		alert(queryString);
+	}
+	else if(chosenMedia =="music"){
+		alert("You have chosen Music");
+	}
+	else if(chosenMedia == "shows"){
+		alert("You have chosen TV Shows");
+	}
+	else if(chosenMedia == "books"){
+		alert("You have chosen Books");
+	}
+	else if(chosenMedia == "movies"){
+		alert("You have chosen Movies");
+	}
+}
+
+//Returns the string of what the user is trying to find.
+function getQueryString(){
+	var queryString = $("#mediaSearch").val();
+	return queryString;
+}
+
+//Gets the value of what type of media the user is searching for.
+function getSelectedMediaValue(){
+	var chosenMedia = $("#mediaChoice").val();
+	return chosenMedia;
+}
