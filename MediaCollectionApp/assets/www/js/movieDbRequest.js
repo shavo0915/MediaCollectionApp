ca85f66f4e4050e10c100319fb425b3e51c7b8d4
aegis42 <aegis42@gmail.com>
2013-02-28 10:49:14 -0700

    Added jquery 1.3.0 libraries and switched html references in index.html to the new jquery libraries. Added a check to see if the selected movie to be added already exists in the user's collection. Also begain sorting the list of movies in the user's collection so the auto-dividers work properly. BONUS: added comments to movieDbRequest for Shavo

--- MediaCollectionApp/assets/www/js/movieDbRequest.js
+++ MediaCollectionApp/assets/www/js/movieDbRequest.js
@@ -5,6 +5,11 @@ var baseAddress;
 var posterSize;
 var myMovies = [];
 
+/**
+ * This function is run when the addMedia page is created and is used to get the base address for the movie posters and the poster sizes
+ * available. Upon a successful call, the function getMovieConfigSuccess is called, otherwise, a popup indicating the service is unavailable
+ * is shown.
+**/
 function getTMDbConfig(){
 	$.ajax({
 		url: "http://api.themoviedb.org/3/configuration",
@@ -21,7 +26,10 @@ function getTMDbConfig(){
 	});
 }
 
-//Uses an ajax call to query the MovieDb and returns a JSON object containing possible matches for the queried movie
+/**
+ * An ajax call is used to query the MovieDb and return a JSON object containing possible matches for the queried movie. The entered movie
+ * title and the api key are sent as data in the ajax call. A successful query will call the getMovieInfoSuccess function
+ **/
 function getMovieInfo(title){
 	query = queryFormat(title);
 	$.ajax({
@@ -36,6 +44,11 @@ function getMovieInfo(title){
 	});
 }
 
+/**
+ * Function handles the returned JSON object from the getMovieInfo ajax call. It first checks if no mathces for the entered movie title were
+ * found. It then populates a listview with the titles and release dates of the movies contained within the JSON object. The JSON is also
+ * saved for use in later functions
+ */
 function getMovieInfoSuccess(data){
 	clearList();
 	//Checks to see if the query returned no results, if so, displays popup indicating no matches were found and exit function
@@ -46,9 +59,6 @@ function getMovieInfoSuccess(data){
 		$('#mediaQueryReturn').popup('open');
 		return;
 	}
-	console.log("Retrieved Movie Info Successfully \n")
-	console.log(data);
-	//console.log(data.results[0].title);
 	
 	movies = data;
 	for(var x in data.results){
@@ -64,26 +74,47 @@ function getMovieInfoSuccess(data){
 		$('#mediaReturn').append(listElement);
 	}
 	
+	//assigns a click listener to each list element in the listview in the mediaQueryReturn popup to hanle the selection of a movie
 	$('#mediaReturn li').click(function() {
 		movieSelect($(this).index());
-	})
+	});
 	$('#mediaReturn').listview( "refresh" );
 	$('#mediaQueryReturn').popup('open');
 }
 
+/**
+ * This function simply takes in the query string from the addMedia page and properly formats it so it can be read by the MovieDb api
+ * @param query
+ * @returns a CGI formatted string (query)
+ */
 function queryFormat(query){
 	query = query.replace(/ /g, "+");
 	console.log(query);
 	return query;
 }
 
-function movieSelect(index){
-	console.log(index);
-	console.log(movies.results[index]);
-	
+/**
+ * Function is called when a user clicks on a movie title in the mediaQueryReturn popup in order to add it to their collecion. It first
+ * checks if the selected movie already exists in the users collection. If not, it adds the movie and then re-sorts the movies alphabetically.
+ * It then assembles the html elements and re-populates the listview on the myMovies page and refreshes it. The user is then notified
+ * that the movie was successfully added to their collection
+ * @param index
+ */
+function movieSelect(index){		
 	var selection = movies.results[index];
+	
+	//this checks if the selected movie already exists in the user's collection
+	for(var i = 0; i < myMovies.length; i++){
+		if(myMovies[i].title == selection.title){
+			$('#mediaQueryReturn').popup('close');
+			setTimeout( function(){ $( '#addExistingMedia' ).popup( 'open' ) }, 100 );
+			return;
+		}			
+	}
+	
 	$('#movieList').empty();
 	
+	//adds the selected movie object to the array of movies representing the user's movie collection and then sorts them alphabetically
 	myMovies.push(selection);
 	myMovies.sort(function (a, b){
 		var titleA = a.title.toLowerCase(), titleB = b.title.toLowerCase();
@@ -94,6 +125,8 @@ function movieSelect(index){
 		return 0;
 	});
 	
+	//Checks if the release date is null and then assembles the appropriate html elements to add to the listview; the movieList 
+	//listview is always cleared and re-populated when a new movie is added
 	for(var x in myMovies){
 		var posterPath = myMovies[x].poster_path;
 		var movieItem = "<li><a href=''><img src=" + baseAddress + posterSize + posterPath + "/><h3>" + myMovies[x].title + "</h3>";
@@ -106,20 +139,29 @@ function movieSelect(index){
 			movieItem = movieItem.concat(elementEnd);
 		}
 		$('#movieList').append(movieItem);
-	}
-	
-	
+	}	
 	
 	$('#movieList').listview("refresh");
 	
+	//the popup is closed and the addMedia confirm popup is opened after giving the mediaQueryReturn popop time to close (neccessary for
+	//new popup to appear
 	$('#mediaQueryReturn').popup('close');
-	$.mobile.changePage( "#myMovies" );
+	setTimeout( function(){ $( '#addMediaConfirm' ).popup( 'open' ) }, 100 );
 }
 
+/**
+ * This function is used to clear the mediaReturn listview
+ */
 function clearList(){
 	$('#mediaReturn').empty();
 }
 
+/**
+ * This fucntion handles the returned JSON object from the getMovieConfif function. It is used to get the base address for the movie
+ * posters and the available sizes for the poster. We assign the base address and the smallest poster size to variables to be used to 
+ * later to assemble the url for the selected movie posters
+ * @param data
+ */
 function getMovieConfigSuccess(data){
 	console.log("Retrieved Config Info Successfully \n")
 	console.log(data);
@@ -128,6 +170,11 @@ function getMovieConfigSuccess(data){
 	posterSize = data.images.poster_sizes[0];
 }
 
+/**
+ * This fucntion is called whenever an ajax call to the MovieDb fails. It opens a popup and notifies the user that there was an error and
+ * that the service is unavailable.
+ * @param status
+ */
 function errorAlert(status){
 	console.log("Error: Service Unavailable"); 
     clearList();
